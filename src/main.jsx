dd0d110771cbc56bed38bfb3b43c452fd37c55fb
bgebes <berkay.gebes@bil.omu.edu.tr>
2022-06-09 13:22:15 +0300

    chore: Deployment and Changing Title

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Helmet from 'react-helmet'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Helmet>
+      <title>Google</title>
+    </Helmet>
     <App />
   </React.StrictMode>
 )
